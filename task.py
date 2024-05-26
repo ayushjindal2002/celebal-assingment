be5f834d7c7345e3e03704e48c666c80d514fc0c
ayush jindal <136922585+ayushjindal2002@users.noreply.github.com>
2024-05-26 12:34:19 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    number1 = int(input())
+    number2 = int(input())
+    # Define two numbers
+
+# Add the numbers
+sum = number1 + number2
+
+# Subtract the numbers
+difference = number1 - number2
+
+# Multiply the numbers
+product = number1 * number2
+
+# Print the results
+print( sum)
+print( difference)
+print( product)
